7a8d0970412dbcf4716c3396b77d67cf9c57e045
Emiliano <guerreroelsa30@gmail.com>
2020-11-16 18:06:11 -0800

    Added an instruction area for the user (5% screen)

---
+++ client/src/instruction.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let instruction = {
+		"status": "on"
+    }
+</script>
+
+<style>
+
+    .instruction-wrapper {
+        text-align: center;
+    }
+
+    fontSize {
+        font-size: 3vh;
+    }
+</style>
+
+<div class="instruction-wrapper">
+    {#if instruction.status == "on"}
+    <fontSize>THIS IS THE INSTRUCTION AREA 5%</fontSize>
+    {/if}
+</div>
