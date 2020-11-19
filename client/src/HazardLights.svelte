244bd4ac6c843f43607293b8b09b4cfbbace20f0
Adam Weingram <adamweingram@gmail.com>
2020-11-19 01:18:04 -0800

    Fixed layout issues in the HazardLights component

--- client/src/HazardLights.svelte
+++ client/src/HazardLights.svelte
@@ -16,40 +16,33 @@
 
 <style>
 	svg {
-		height: 34%;
-		width: 38%;
-		position: absolute;
-		position: absolute;
-		left: 42%;
-		top: -7%;
+		margin: auto;
+		height: 100%;
+		width: 100%;
 	}
 
-	.hazOff {
-		height: 90%;
-		width: 90%;
-		position: absolute;
-		left: 19%;
-		top: -15%;
-		transform: scale(0.7);
+	.hazard-light-wrapper {
+		display: flex;
+		height: 100%;
+		width: 100%;
 	}
 </style>
 
-<div class="image-wrapper">
+<div class="hazard-light-wrapper">
 	{#if hazard1}
-		<div class="hazOff">
-			<svg>
-				xmlns:dc="http://purl.org/dc/elements/1.1/"
-				xmlns:cc="http://creativecommons.org/ns#"
-				xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-				xmlns:svg="http://www.w3.org/2000/svg"
-				xmlns="http://www.w3.org/2000/svg"
-				xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-				xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-				width="210mm" height="297mm" viewBox="0 0 210 297" version="1.1"
-				id="svg8" inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"
-				sodipodi:docname="Hazard Lights.svg">
-				<defs id="defs2" />
-				<sodipodi:namedview
+		<svg viewBox="0 0 220 270">
+			xmlns:dc="http://purl.org/dc/elements/1.1/"
+			xmlns:cc="http://creativecommons.org/ns#"
+			xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+			xmlns:svg="http://www.w3.org/2000/svg"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+			xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+<!--			width="210mm" height="297mm" viewBox="0 0 210 297" version="1.1"-->
+			id="svg8" inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"
+			sodipodi:docname="Hazard Lights.svg">
+			<defs id="defs2" />
+			<sodipodi:namedview
 					id="base"
 					pagecolor="#ffffff"
 					bordercolor="#666666"
@@ -71,23 +64,23 @@
 					showguides="true"
 					objecttolerance="10"
 					inkscape:pagecheckerboard="true">
-					<inkscape:grid type="xygrid" id="grid14" />
-				</sodipodi:namedview>
-				<metadata id="metadata5">
-					<rdf:RDF>
-						<cc:Work rdf:about="">
-							<dc:format>image/svg+xml</dc:format>
-							<dc:type
+				<inkscape:grid type="xygrid" id="grid14" />
+			</sodipodi:namedview>
+			<metadata id="metadata5">
+				<rdf:RDF>
+					<cc:Work rdf:about="">
+						<dc:format>image/svg+xml</dc:format>
+						<dc:type
 								rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-							<dc:title />
-						</cc:Work>
-					</rdf:RDF>
-				</metadata>
-				<g
+						<dc:title />
+					</cc:Work>
+				</rdf:RDF>
+			</metadata>
+			<g
 					inkscape:label="Layer 1"
 					inkscape:groupmode="layer"
 					id="layer1">
-					<path
+				<path
 						sodipodi:type="star"
 						style="fill:#ff0000;stroke-width:0.264583"
 						id="path20"
@@ -105,12 +98,12 @@
 						inkscape:transform-center-x="0.19625343"
 						inkscape:transform-center-y="11.339281"
 						transform="scale(-1)" />
-				</g>
-				<g
+			</g>
+			<g
 					inkscape:groupmode="layer"
 					id="layer2"
 					inkscape:label="Layer 2">
-					<path
+				<path
 						sodipodi:type="star"
 						style="fill:#000000;stroke-width:0.206711"
 						id="path20-5"
@@ -128,12 +121,12 @@
 						inkscape:transform-center-x="0.15331697"
 						inkscape:transform-center-y="8.859055"
 						transform="scale(-1)" />
-				</g>
-				<g
+			</g>
+			<g
 					inkscape:groupmode="layer"
 					id="layer3"
 					inkscape:label="Layer 3">
-					<path
+				<path
 						sodipodi:type="star"
 						style="fill:#ff0000;stroke-width:0.147668"
 						id="path20-5-6"
@@ -151,12 +144,12 @@
 						inkscape:transform-center-x="0.10952439"
 						inkscape:transform-center-y="6.3286543"
 						transform="scale(-1)" />
-				</g>
-				<g
+			</g>
+			<g
 					inkscape:groupmode="layer"
 					id="layer4"
 					inkscape:label="Layer 4">
-					<path
+				<path
 						sodipodi:type="star"
 						style="fill:#000000;stroke-width:0.118904"
 						id="path20-5-5"
@@ -174,11 +167,10 @@
 						inkscape:transform-center-x="0.08819325"
 						inkscape:transform-center-y="5.0959003"
 						transform="scale(-1)" />
-				</g>
-			</svg>
-		</div>
+			</g>
+		</svg>
 	{:else}
-		<svg
+		<svg viewBox="0 0 220 270">
 			xmlns:dc="http://purl.org/dc/elements/1.1/"
 			xmlns:cc="http://creativecommons.org/ns#"
 			xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
