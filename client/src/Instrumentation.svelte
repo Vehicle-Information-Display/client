4947ee5cfdf8b1644ba8be9df016900f0e73a0b4
Manvir Dhillon <manvir.dhillon30@gmail.com>
2020-12-04 23:37:41 -0800

    created json object for car direction/time

--- client/src/Instrumentation.svelte
+++ client/src/Instrumentation.svelte
@@ -10,6 +10,24 @@
     // Handle a standard-format message
     export function handleMessage(message) {
         // [TODO] Implement instrumentation message handler
+        var actions = [];
+        var temp = new Object();
+        if(message.name == "vehicleright"){
+            //console.log("Moved Right @ " + message.timestamp);
+            temp["direction"] = "right";
+            temp["time"] = message.timestamp;
+            actions.push(temp);
+            //console.log(actions);
+        }
+        else if(message.name == "vehicleleft"){
+            //onsole.log("Moved Left @ " + message.timestamp);
+            temp["direction"] = "left";
+            temp["time"] = message.timestamp;
+            actions.push(temp);
+            //console.log(actions);
+        }
+        console.log(message);
+
     }
 
     // Handle a global simulation tick
